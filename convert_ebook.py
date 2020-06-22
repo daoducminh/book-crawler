32b357e0a0e218e20d087d8119a7643a308664d8
daoducminh <daoducminh1997@gmail.com>
2020-06-22 10:50:02 +0700

    Update convert_ebook.py

--- convert_ebook.py
+++ convert_ebook.py
@@ -4,11 +4,10 @@ from book_list import book_list
 
 FILENAME = '{}.html'
 # Ebook convert commands
-HTML_AZW3 = 'tsp ebook-convert books/{0}.html books/{0}.azw3 --level1-toc "//h:h2" --no-inline-toc'
+HTML_AZW3 = 'ebook-convert books/{0}.html books/{0}.azw3 --level1-toc "//h:h2" --no-inline-toc'
 
 
 def main():
-    system('cd books/')
     for book in book_list:
         # Convert HTML file to AZW3 file
         system(HTML_AZW3.format(book))

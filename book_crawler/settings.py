23b13187c0726e55d7fb1542de0e397e3f8324f3
daoducminh <daoducminh1997@gmail.com>
2020-06-30 16:19:34 +0700

    Update settings

--- book_crawler/settings.py
+++ book_crawler/settings.py
@@ -23,7 +23,7 @@ ROBOTSTXT_OBEY = False
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 512
 CONCURRENT_ITEMS = 500
-
+RETRY_TIMES = 10
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs

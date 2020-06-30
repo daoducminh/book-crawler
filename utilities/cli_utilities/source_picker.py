037bfc9574914b9468634d814c5794369d37dc6c
daoducminh <daoducminh1997@gmail.com>
2020-06-30 17:41:25 +0700

    Update sourc picker

--- utilities/cli_utilities/source_picker.py
+++ utilities/cli_utilities/source_picker.py
@@ -1,10 +1,15 @@
+import pickle
 from book_lists import book_list_yy, book_list_ttv, book_list_tfull, book_list_tcv
 
 
 def select_source(x):
+    file = open('book_lists/fullyy.pkl', 'rb')
+    top_book_list = pickle.load(file)
+    file.close()
     return {
         'yy': book_list_yy.book_list,
         'ttv': book_list_ttv.book_list,
         'tcv': book_list_tcv.book_list,
-        'tfull': book_list_tfull.book_list
+        'tfull': book_list_tfull.book_list,
+        'fullyy': top_book_list
     }[x]

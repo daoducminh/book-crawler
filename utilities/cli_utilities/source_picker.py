943a17346effb89e27de049e7f2be27be4225533
daoducminh <daoducminh1997@gmail.com>
2020-07-01 15:15:38 +0700

    Update cli

--- utilities/cli_utilities/source_picker.py
+++ utilities/cli_utilities/source_picker.py
@@ -1,15 +1,23 @@
-import pickle
 from book_lists import book_list_yy, book_list_ttv, book_list_tfull, book_list_tcv
+from os import getenv
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='.env')
+MONGODB_URI = getenv('MONGODB_URI')
+
+
+def get_top_book_list(name):
+    client = MongoClient(MONGODB_URI)
+    db = client[name]
+    return db.list_collection_names()
 
 
 def select_source(x):
-    file = open('book_lists/fullyy.pkl', 'rb')
-    top_book_list = pickle.load(file)
-    file.close()
     return {
         'yy': book_list_yy.book_list,
         'ttv': book_list_ttv.book_list,
         'tcv': book_list_tcv.book_list,
         'tfull': book_list_tfull.book_list,
-        'fullyy': top_book_list
+        'fullyy': get_top_book_list('fullyy')
     }[x]

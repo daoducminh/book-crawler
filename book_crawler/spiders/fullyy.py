adac697eb0c4413d351b4826bf30a92cf9f763f3
daoducminh <daoducminh1997@gmail.com>
2020-07-01 15:22:17 +0700

    Remove cookie from spider

--- book_crawler/spiders/fullyy.py
+++ book_crawler/spiders/fullyy.py
@@ -23,7 +23,7 @@ class FullYYSpider(YYSpider):
         'LOG_ENABLED': False,
         'DEFAULT_REQUEST_HEADERS': {
             'accept': '*/*',
-            'cookie': COOKIE,
+            # 'cookie': COOKIE,
             'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
         }
     }

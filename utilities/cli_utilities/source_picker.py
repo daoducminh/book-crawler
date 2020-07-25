aac63398852ed17944e342cb6636ef1a8d8a429d
daoducminh <daoducminh1997@gmail.com>
2020-07-25 08:38:14 +0700

    Update yy domain name

--- utilities/cli_utilities/source_picker.py
+++ utilities/cli_utilities/source_picker.py
@@ -4,13 +4,13 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path='.env')
-MONGODB_URI = getenv('MONGODB_URI')
+# MONGODB_URI = getenv('MONGODB_URI')
 
 
-def get_top_book_list(name):
-    client = MongoClient(MONGODB_URI)
-    db = client[name]
-    return db.list_collection_names()
+# def get_top_book_list(name):
+#     client = MongoClient(MONGODB_URI)
+#     db = client[name]
+#     return db.list_collection_names()
 
 
 def select_source(x):
@@ -19,5 +19,5 @@ def select_source(x):
         'ttv': book_list_ttv.book_list,
         'tcv': book_list_tcv.book_list,
         'tfull': book_list_tfull.book_list,
-        'fullyy': get_top_book_list('fullyy')
+        # 'fullyy': get_top_book_list('fullyy')
     }[x]

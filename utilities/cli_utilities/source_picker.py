4c41808df5df79a3519ae1e1d1b68bb2dbc17784
Minh Dao <minh.dao@jmango360.com>
2020-08-06 16:40:44 +0700

    Fix mongoDB failed connection

--- utilities/cli_utilities/source_picker.py
+++ utilities/cli_utilities/source_picker.py
@@ -1,16 +1,8 @@
 from book_lists import book_list_yy, book_list_ttv, book_list_tfull, book_list_tcv
 from os import getenv
-from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path='.env')
-MONGODB_URI = getenv('MONGODB_URI')
-
-
-def get_top_book_list(name):
-    client = MongoClient(MONGODB_URI)
-    db = client[name]
-    return db.list_collection_names()
 
 
 def select_source(x):
@@ -19,5 +11,5 @@ def select_source(x):
         'ttv': book_list_ttv.book_list,
         'tcv': book_list_tcv.book_list,
         'tfull': book_list_tfull.book_list,
-        'fullyy': get_top_book_list('fullyy')
+        # 'fullyy': get_top_book_list('fullyy')
     }[x]
--- utilities/cli_utilities/transfer_books.py
+++ utilities/cli_utilities/transfer_books.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path='.env')
 VPS = getenv('VPS')
 PRIVATE_KEY = getenv('PRIVATE_KEY')
-DOWNLOAD_BOOKS_COMMAND = 'scp -i {2} {1}:~/book-crawler/books/{0}.azw3 ./books'
+DOWNLOAD_BOOKS_COMMAND = 'scp -i {2} {1}:~/book-crawler/books/{0}.* ./books'
 UPLOAD_BOOK_LIST_COMMAND = 'scp -i {2} ./book_lists/book_list_{0}.py {1}:~/book-crawler/book_lists'
 
 

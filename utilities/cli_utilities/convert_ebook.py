e76c42247b91962accec198abdfb0d9aab8f6520
daoducminh <daoducminh1997@gmail.com>
2020-08-26 17:20:42 +0700

    Add epub conversion command

--- utilities/cli_utilities/convert_ebook.py
+++ utilities/cli_utilities/convert_ebook.py
@@ -2,10 +2,12 @@ from os import system
 import sys
 import re
 
-HTML_AZW3 = 'tsp ebook-convert books/{0}.html books/{0}.azw3 --level1-toc "//h:h1" --no-inline-toc'
+AZW3 = 'azw3'
+EPUB = 'epub'
+CONVERT_CMD = 'tsp ebook-convert books/{0}.html books/{0}.{1} --level1-toc "//h:h1" --no-inline-toc'
 
 
 def convert_ebook(book_list):
     for book in book_list:
-        # Convert HTML file to AZW3 file
-        system(HTML_AZW3.format(book))
+        system(CONVERT_CMD.format(book, AZW3))
+        system(CONVERT_CMD.format(book, EPUB))

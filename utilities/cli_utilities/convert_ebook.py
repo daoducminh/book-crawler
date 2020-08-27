a4b9e15a06ad2a2f513257ac985b0860254e34b3
daoducminh <daoducminh1997@gmail.com>
2020-08-27 10:00:13 +0700

    Fix conversion command for epub

--- utilities/cli_utilities/convert_ebook.py
+++ utilities/cli_utilities/convert_ebook.py
@@ -4,10 +4,11 @@ import re
 
 AZW3 = 'azw3'
 EPUB = 'epub'
-CONVERT_CMD = 'tsp ebook-convert books/{0}.html books/{0}.{1} --level1-toc "//h:h1" --no-inline-toc'
+NO_INLINE_TOC = '--no-inline-toc'
+CONVERT_CMD = 'tsp ebook-convert books/{0}.html books/{0}.{1} --level1-toc "//h:h1" {2}'
 
 
 def convert_ebook(book_list):
     for book in book_list:
-        system(CONVERT_CMD.format(book, AZW3))
-        system(CONVERT_CMD.format(book, EPUB))
+        system(CONVERT_CMD.format(book, AZW3, NO_INLINE_TOC))
+        system(CONVERT_CMD.format(book, EPUB, ''))

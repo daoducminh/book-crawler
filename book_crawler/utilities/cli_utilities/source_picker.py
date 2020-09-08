24c7e2f518b015b50932b3bb491cce967265d705
daoducminh <daoducminh1997@gmail.com>
2020-09-08 08:54:06 +0700

    Update source_picker

--- book_crawler/utilities/cli_utilities/source_picker.py
+++ book_crawler/utilities/cli_utilities/source_picker.py
@@ -1,4 +1,4 @@
-from book_lists import book_list_yy, book_list_ttv, book_list_tfull, book_list_tcv
+from book_lists import book_list_yy, book_list_ttv, book_list_tfull, book_list_tcv, book_list_tct
 from os import getenv
 from dotenv import load_dotenv
 
@@ -11,5 +11,6 @@ def select_source(x):
         'ttv': book_list_ttv.book_list,
         'tcv': book_list_tcv.book_list,
         'tfull': book_list_tfull.book_list,
+        'tct': book_list_tct.book_list
         # 'fullyy': get_top_book_list('fullyy')
     }[x]

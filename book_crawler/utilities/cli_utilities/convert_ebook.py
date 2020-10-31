92ae3ea8b5ceea47b59ada6fb6cc3ff7ce58c61d
daoducminh <daoducminh1997@gmail.com>
2020-10-31 12:04:50 +0700

    Fix minor bug

--- book_crawler/utilities/cli_utilities/convert_ebook.py
+++ book_crawler/utilities/cli_utilities/convert_ebook.py
@@ -6,11 +6,12 @@ AZW3 = 'azw3'
 EPUB = 'epub'
 NO_INLINE_TOC = '--no-inline-toc'
 CONVERT_CMD = 'tsp ebook-convert books/{0}/{1}.html books/{0}/{1}.{2} --level1-toc "//h:h1" {3}'
+BOOK_DIR = 'books/{}'
 
 
 def convert_ebook(book_list):
-    for file in listdir('books/'):
-        for book in book_list:
+    for book in book_list:
+        for file in listdir(BOOK_DIR.format(book)):
             if book in file:
                 file_name = file[:-5]
                 system(CONVERT_CMD.format(book, file_name, AZW3, NO_INLINE_TOC))

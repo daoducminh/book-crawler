10f08e9bfa1a42ff063a91fe889c300292f56d4d
daoducminh <daoducminh1997@gmail.com>
2020-10-31 11:02:19 +0700

    Update convert.sh

--- book_crawler/utilities/cli_utilities/convert_ebook.py
+++ book_crawler/utilities/cli_utilities/convert_ebook.py
@@ -2,7 +2,7 @@
 
 from os import system, listdir
 
-CONVERT_CMD = 'tsp ./convert.sh {0} {1} {2}'
+CONVERT_CMD = 'tsp ./convert.sh {0} {1} {2} {3}'
 AZW3 = 'azw3'
 EPUB = 'epub'
 NO_INLINE_TOC = '--no-inline-toc'
--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ebook-convert books/$1/$1.html books/$1/$1.$2 --level1-toc "//h:h1" $3 && cp -rf books/$1 ../gdrive/My\ Drive/Ebook/
+ebook-convert books/$1/$2.html books/$1/$2.$3 --level1-toc "//h:h1" $4 && cp -rf books/$1 ../gdrive/My\ Drive/Ebook/

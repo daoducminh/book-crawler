fc171721ec5bebfe9262623f412210f3eea025de
daoducminh <daoducminh1997@gmail.com>
2020-11-07 22:48:35 +0700

    [New] Add mtc spider

--- book_crawler/utilities/cli_utilities/source_picker.py
+++ book_crawler/utilities/cli_utilities/source_picker.py
@@ -1,18 +1,38 @@
 # -*- coding: utf-8 -*-
+# from book_lists import book_list_yy, book_list_ttv, book_list_tfull, book_list_tcv, book_list_tct
 
-from dotenv import load_dotenv
-
-from book_lists import book_list_yy, book_list_ttv, book_list_tfull, book_list_tcv, book_list_tct
-
-load_dotenv(dotenv_path='.env')
+# def select_source(x):
+#     return {
+#         'yy': book_list_yy.book_list,
+#         'ttv': book_list_ttv.book_list,
+#         'tcv': book_list_tcv.book_list,
+#         'tfull': book_list_tfull.book_list,
+#         'tct': book_list_tct.book_list
+#         # 'fullyy': get_top_book_list('fullyy')
+#     }[x]
 
 
 def select_source(x):
-    return {
-        'yy': book_list_yy.book_list,
-        'ttv': book_list_ttv.book_list,
-        'tcv': book_list_tcv.book_list,
-        'tfull': book_list_tfull.book_list,
-        'tct': book_list_tct.book_list
-        # 'fullyy': get_top_book_list('fullyy')
-    }[x]
+    if x == 'yy':
+        from book_lists.book_list_yy import book_list
+        return book_list
+    elif x == 'ttv':
+        from book_lists.book_list_ttv import book_list
+        return book_list
+    elif x == 'tcv':
+        from book_lists.book_list_tcv import book_list
+        return book_list
+    elif x == 'tct':
+        from book_lists.book_list_tct import book_list
+        return book_list
+    elif x == 'tfull':
+        from book_lists.book_list_tfull import book_list
+        return book_list
+    elif x == 'mtc':
+        from book_lists.book_list_mtc import book_list
+        return book_list
+    # elif x == '':
+    #     from book_lists.book_list_ import book_list
+    #     return book_list
+    else:
+        return None
